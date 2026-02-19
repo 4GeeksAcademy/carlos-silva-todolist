7467b1870c89538da197162467feececbb3d6cc8
Carlos Silva <carlhmath@gmail.com>
2026-02-19 00:20:54 +0000

    entrego proyecto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -12,45 +12,47 @@ const Home = () => {
 		setInputValue(event.target.value)
 
 	}
-	function addTask(event){
-         if (event.key=="Enter"){
+	function addTask(event) {
+		if (event.key == "Enter") {
 			setTodos([
 				...todos, inputValue
 			])
-		 }
+			setInputValue("")
+		}
 	}
-	function deleteTask (id) {
-		const newTodos = todos.filter((item, index) => index != id )
-		setTodos (newTodos)
+	function deleteTask(id) {
+		const newTodos = todos.filter((item, index) => index != id)
+		setTodos(newTodos)
 	}
 	return (
 		<div className="container">
-			<h1>MY TODOS </h1>
-			<ul>
-				<li>
-					<input type="text"
-						onChange={handleChange}
-						value={inputValue}
-                        onKeyDown={addTask}
-						placeholder="Whats do you need to do?" />
-				</li>
-                {  
-				  todos.map( (task, index) => (
-					    <li>
-							{task} 
-							<span onClick={() => deleteTask (index) }>
-								<i class="fa-solid fa-trash" ></i>
-							</span> 
-					   </li>
-				  ) )
-				}
-           				
-		
-					
-			
-			</ul>
-			<div>23 tasks</div>
+			<div className="row justify-content-center">
+				<div className="col-12 col-md-7">
 
+					<h1> To Do List </h1>
+					<ul>
+						<li>
+							<input type="text"
+								onChange={handleChange}
+								value={inputValue}
+								onKeyDown={addTask}
+								placeholder="Whats do you need to do?" />
+						</li>
+						{
+							todos.map((task, index) => (
+								<li>
+									{task}
+									<span onClick={() => deleteTask(index)}>
+										<i class="fa-solid fa-trash" ></i>
+									</span>
+								</li>
+							))
+						}
+
+					</ul>
+					<div> {todos.length} tasks</div>
+				</div>
+			</div>
 		</div >
 	);
 };
